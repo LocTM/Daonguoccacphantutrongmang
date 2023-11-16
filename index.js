a4563de29007d03219106ee3a52cdd3b2b742d1f
LocTM <tranminhloc.anhk@gmail.com>
2023-11-16 09:01:16 +0700

    main

---
+++ index.js
@@ -0,0 +1,22 @@
+let x = [1,2,3,4,5,6,7,8,9,10];
+//Cach 1
+// let first = 0;
+// let last = x.length -1;
+// while (first < last) {
+//     let b = x[first];
+//     x[first] = x[last];
+//     x[last] = b;
+//     first++;
+//     last--;
+// }
+// document.write(x)
+
+
+//Cach2
+// x.sort(function (a,b){
+//     return b-a;
+// })
+// document.write(x)
+
+//Cach3
+document.write(x.reverse())
